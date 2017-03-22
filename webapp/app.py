65837c3c798b361f12fd9ea9c55c05c844a4782b
Hunter <hbingham@ucsc.edu>
2017-03-21 20:26:53 -0700

    project4.1

--- webapp/app.py
+++ webapp/app.py
@@ -3,13 +3,52 @@ from flask_api import status
 app = Flask(__name__)
 import os
 import requests as req
+import time
 theDict = dict()
 IPPORT = os.getenv('IPPORT')
+K = int(os.getenv('K'))
+timestamp = 0
+partitionList = list()
 
 if 'VIEW' in os.environ:
 	ip_view = os.getenv('VIEW').split(',')
 else:
 	ip_view = list()
+
+
+
+
+@app.route("/kvs/get_partition_id")
+def getPartitionID():
+	indexIP = ip_view.index(IPPORT)
+	partitionID = int(indexIP/K)
+	retDict = {"msg":"success","partition_id": str(partitionID)}
+	return jsonify(retDict), status.HTTP_200_OK
+
+@app.route("/kvs/get_all_partition_ids")
+def getPartitionIDList():
+	partitions = int(int(len(ip_view))/K)
+	retList = list()
+	for i in range(0,partitions):
+		retList.append(i)
+	retDict = {"msg":"success","partition_id_list":retList}
+	return jsonify(retDict), status.HTTP_200_OK
+
+
+@app.route("/kvs/get_partition_members")
+def getAllPartitionIDs():
+	partitionID = int(request.form.get("partition_id"))
+	partitionNodes = list()
+	startIndex = partitionID * K
+	if int(len(ip_view)) < startIndex + K:
+		endIndex = int(len(ip_view))
+	else:
+		endIndex = startIndex + K
+	for i in range(startIndex, endIndex):
+		partitionNodes.append(ip_view[i])
+	retDict = {"msg":"success","partition_members":partitionNodes}
+	return jsonify(retDict), status.HTTP_200_OK
+	
 
 
 @app.route("/kvs/view_update", methods=["PUT"])
@@ -28,17 +67,19 @@ def view_update():
 		if node in hashView:
 			hashView.remove(node)
 
-	
-	for server in ip_view: # update every server in this servers IP view with new node
-		if server != IPPORT: # send a new request to server
+	#Set all other ip_views to updated ip_view
+	for server in ip_view: 
+		if server != IPPORT: 
 			req.put("http://" + server + "/kvs/removeView", timeout=1)
 			for server2 in hashView:
 				response = req.put("http://" + server + "/kvs/add/" + server2, timeout=1)
 
+	#Update target node ipview
 	req.put("http://" + node + "/kvs/removeView", timeout=1)
 	for server in hashView:
 		response = req.put("http://" + node + "/kvs/add/" + server, timeout=1)
 
+	#Rehash all other data
 	someDict = dict()
 	theLen = int(len(hashView))
 	for server in ip_view:
@@ -50,34 +91,24 @@ def view_update():
 				nodeLocate = ryan_hash(key) % theLen
 				theValue = someDict[key]
 				if IPPORT != hashView[nodeLocate]:
-					print("server: " + hashView[nodeLocate])
-					print("key: " + key)
 					url_str = 'http://' + hashView[nodeLocate] + '/kvs/' + key
 					someRes = req.put(url_str,data={'val':theValue}, timeout=6)
 				else:
 					theDict[key] = theValue
-
+	#Rehash local data
 	delList = list()
-	print("yee")
 	for key in theDict:
 		nodeLocate = ryan_hash(key) % theLen
-		print("key: " + key)
 		if IPPORT != hashView[nodeLocate]:
 			theValue = theDict[key]
 			url_str = "http://" + hashView[nodeLocate] + "/kvs/" + key
-			print("IPPort local dicT: " + hashView[nodeLocate] + " value: " + theValue)
 			req.put(url_str,data={'val':theValue}, timeout=5)
-			print("we here1")
 			delList.append(key)
-		else:
-			print("yoyooo")
-
-	print("we here2")
+	#Delete rehashed data that isn't wanted here
 	for key in delList:
 		del theDict[key]
-		
-	print("we here3")	
 
+	#Update local ip_view
 	if type == "remove":
 		someRes = req.get("http://" + node + "/kvs/delDict", timeout=5)
 		if node in ip_view:
@@ -92,46 +123,32 @@ def view_update():
 	return jsonify(retDict), status.HTTP_200_OK
 
 
-@app.route('/kvs/<key>', methods = ['GET', 'PUT','DELETE']) 
+
+@app.route('/kvs/<key>', methods = ['GET', 'PUT']) 
 def index(key):
+	strClock = request.form.get('causal_payload')
+	if '.' in strClock:
+		inClock = strClock.split('.')
+	else:
+		inClock = dict()
 	theLen = int(len(ip_view))
-	#print('hash: ' + str(ryan_hash(key)))
-	#print('keyX' + str(key) + 'X')
 	nodeLocate = ryan_hash(key) % theLen
-	#print ("IPPORT: " + IPPORT + " ip_view: " + ip_view[nodeLocate])
-	#print("nodelocate: " + str(nodeLocate))
-	#print("len: " + str(theLen))
-	print("nodeLocater: " + ip_view[nodeLocate] + " len: " + str(theLen))
 	if ip_view[nodeLocate] == IPPORT:
 		if request.method == 'PUT':
-			#print('put')
 			if key in theDict:
-				#print('indict')
 				content = 'replaced: 1, // 0 if key did not exist msg: success\n'
 				helpRet = 1
 			else:
 				content = 'replaced: 0, // 1 if an existing keys val was replaced\n msg success\n'
-				#print('not in dict')
 				helpRet = 0
 			theValue = request.form.get('val')
-			#print('val: ' + str(theValue))
 			theDict[key] = theValue
 			if helpRet == 1:
 				retDict = {'replaced':'1, // 0 if key did not exist','msg':'success', 'owner':ip_view[nodeLocate]}
 				return jsonify(retDict), status.HTTP_200_OK
 			else:
 				retDict = {'replaced':'0, // 1 if an existing keys val was replaced','msg':'success', 'owner':ip_view[nodeLocate]}
-				return jsonify(retDict), status.HTTP_201_CREATED
-		elif request.method == 'DELETE':
-			if key in theDict:
-				del theDict[key]
-				content = 'msg : success\n'
-				retDict = {'msg':'success','owner':ip_view[nodeLocate]}
-				return jsonify(retDict), status.HTTP_200_OK
-			else:
-				content = 'msg : error\nerror : key does not exist\n'
-				retDict = {'msg':'error','error':'key does not exist'}
-				return jsonify(retDict), status.HTTP_404_NOT_FOUND	
+				return jsonify(retDict), status.HTTP_201_CREATED	
 		elif request.method == 'GET':
 			if key in theDict:
 				theRet = theDict[key]
@@ -141,14 +158,12 @@ def index(key):
 				retDict = {'msg':'error', 'error':'key does not exist'}
 				return jsonify(retDict),status.HTTP_404_NOT_FOUND
 		else:
-			return jsonify('Error: Please call a GET, PUT or DELETE method.\n')
+			return jsonify('Error: Please call a GET or PUT method.\n')
 	else:
 		url_str = 'http://' + ip_view[nodeLocate] + '/kvs/' + key
-		#print('urlstr: ' + url_str)
 		if request.method == 'PUT':
 			theValue = request.form.get('val')
 			req.data = theValue
-			#print('value: ' + theValue)
 			resp = req.put(url_str,data=dict(val=theValue))
 			dRet = json.loads(resp.content)
 			return jsonify(dRet), resp.status_code
@@ -156,10 +171,8 @@ def index(key):
 			resp = req.get(url_str)
 			dRet = json.loads(resp.content)
 			return jsonify(dRet), resp.status_code
-		elif request.method == 'DELETE':
-			resp = req.delete(url_str)
-			dRet = json.loads(resp.content)
-			return jsonify(dRet), resp.status_code
+
+
 
 @app.route("/kvs/add/<theIP>", methods=["PUT"])
 def add(theIP):
@@ -188,6 +201,19 @@ def delDict():
 	theDict = dict()
 	return jsonify("del complete")
 
+def compareClocks(thisClock,incClock):
+	for key in incClock:
+		if key in thisClock:
+			if incClock[key] > thisClock[key]:
+				thisClock[key] = incClock[key]
+	return thisClock
+
+def ping(ip):
+	resp = os.system("ping -c 1 " +  ip[:-5])
+	if resp == 0:
+		return True
+	else:
+		return False
 
 def ryan_hash(some_string):
     hashvalue = 0
@@ -202,4 +228,4 @@ def ryan_hash(some_string):
 
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 8080))
-	app.run(host='0.0.0.0' , port=port)
+	app.run(host='0.0.0.0' , port=port, threaded=True)
